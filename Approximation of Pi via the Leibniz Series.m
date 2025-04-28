db95a7e9afa31d440c36ef3d1dde8de32f9b5ee4
EBUBEKİR AKIN <117471370+ebubekirakin@users.noreply.github.com>
2025-04-28 18:02:13 +0300

    Add files via upload

---
+++ Approximation of Pi via the Leibniz Series.m
@@ -0,0 +1,17 @@
+clear all; close all; clc
+x=1;
+toplam=0
+pi=4*atan(1);
+for n=1:100
+    isaret=(-1)^(n+1);
+    pay=x^(2*n-1);
+    payda=2*n-1;
+    sontop=toplam+4*isaret*pay/payda;
+    truehata=abs(pi-sontop)/abs(pi);
+    yakhata=abs(sontop-toplam)/abs(sontop);
+    plot(n,yakhata,'--r*',n,truehata,'--b+');
+    hold on;
+    xlabel('n terim sayısı');
+    ylabel('hata');
+   toplam=sontop;
+end
