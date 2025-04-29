799aa7736a3ad901562c983dfdb350f201410428
EBUBEKİR AKIN <117471370+ebubekirakin@users.noreply.github.com>
2025-04-29 16:22:50 +0300

    Update Approximation of Pi via the Leibniz Series.m

--- Approximation of Pi via the Leibniz Series.m
+++ Approximation of Pi via the Leibniz Series.m
@@ -2,7 +2,9 @@ clear all; close all; clc
 x=1;
 toplam=0
 pi=4*atan(1);
+
 for n=1:100
+   
     isaret=(-1)^(n+1);
     pay=x^(2*n-1);
     payda=2*n-1;
@@ -14,4 +16,5 @@ for n=1:100
     xlabel('n terim sayısı');
     ylabel('hata');
    toplam=sontop;
+   
 end
